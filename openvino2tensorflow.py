5c85e312b105c5e7623f5e4d941334329e97e1da
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-10-21 02:16:44 +0900

    trial and error

--- openvino2tensorflow.py
+++ openvino2tensorflow.py
@@ -48,6 +48,11 @@ python3 openvino2tensorflow.py \
   --model_path=openvino/midasnet/FP32/midasnet.xml \
   --output_saved_model=True \
   --output_no_quant_float32_tflite=True
+
+python3 openvino2tensorflow.py \
+  --model_path openvino/tf_efficientnet_lite3_256x256/FP32/tf_efficientnet_lite3.xml \
+  --output_saved_model=True \
+  --output_pb=True
 '''
 
 import os
@@ -483,7 +488,7 @@ def convert(model,
             else:
                 tf_layers_dict[layer_id] = tf.transpose(tf_layers_dict[tf_edges[layer_id][0]], perm=tf_layers_dict[tf_edges[layer_id][1]])
 
-        ### Squeeze - TODO
+        ### Squeeze
         elif layer.attrib['type'] == 'Squeeze':
             if transpose_squeeze_skip:
                 continue
@@ -507,32 +512,15 @@ def convert(model,
 
         ### Gather - TODO
         elif layer.attrib['type'] == 'Gather':
-            axis = None
-            if len(tf_layers_dict[tf_edges[layer_id][2]]) == 1:
-                axis = int(tf_layers_dict[tf_edges[layer_id][2]])
-                if axis == 1:
-                    axis = -1
-                elif axis >= 2:
-                    axis -= 1
-            else:
-                for idx, part_axis in enumerate(tf_layers_dict[tf_edges[layer_id][2]]):
-                    if part_axis == 1:
-                        tf_layers_dict[tf_edges[layer_id][2]][idx] = 3
-                    elif part_axis >= 2:
-                        tf_layers_dict[tf_edges[layer_id][2]][idx] -= 1
-                axis = tf_layers_dict[tf_edges[layer_id][2]]
-            tf_layers_dict[layer_id] = tf.gather(tf_layers_dict[tf_edges[layer_id][0]], int(tf_layers_dict[tf_edges[layer_id][1]]), axis=tf_layers_dict[tf_edges[layer_id][2]])
+            axis = int(tf_layers_dict[tf_edges[layer_id][2]])
+            indices = int(tf_layers_dict[tf_edges[layer_id][1]])
+            tf_layers_dict[layer_id] = [tf.cast(tf.gather(tf_layers_dict[tf_edges[layer_id][0]], indices, axis=axis), tf.int64)]
 
         ### ReduceMean, ReduceMax, ReduceMin, ReduceSum
         elif layer.attrib['type'] == 'ReduceMean' or layer.attrib['type'] == 'ReduceMax' or layer.attrib['type'] == 'ReduceMin' or layer.attrib['type'] == 'ReduceSum':
             keep_dims = True if data.attrib['keep_dims'] == "True" else False
-            if (type(tf_layers_dict[tf_edges[layer_id][1]]) == np.ndarray) and len(tf_layers_dict[tf_edges[layer_id][1]]) == 2 and \
-               ((tf_layers_dict[tf_edges[layer_id][1]][0] == 3 and tf_layers_dict[tf_edges[layer_id][1]][1] == 2) or (tf_layers_dict[tf_edges[layer_id][1]][0] == 2 and tf_layers_dict[tf_edges[layer_id][1]][1] == 3)):
-                axis1 = tf_layers_dict[tf_edges[layer_id][1]][0] - 1
-                axis2 = tf_layers_dict[tf_edges[layer_id][1]][1] - 1
-            else:
-                axis1 = tf_layers_dict[tf_edges[layer_id][1]][0]
-                axis2 = tf_layers_dict[tf_edges[layer_id][1]][1]
+            axis1 = tf_layers_dict[tf_edges[layer_id][1]][0] - 1
+            axis2 = tf_layers_dict[tf_edges[layer_id][1]][1] - 1
             if layer.attrib['type'] == 'ReduceMean':
                 tf_layers_dict[layer_id] = tf.math.reduce_mean(tf_layers_dict[tf_edges[layer_id][0]], axis=[axis1, axis2], keepdims=keep_dims)
             elif layer.attrib['type'] == 'ReduceMax':
@@ -562,8 +550,10 @@ def convert(model,
         ### Range - TODO
         elif layer.attrib['type'] == 'Range':
             dtype = cast_type_ov_tf[data.attrib['output_type']]
-            tf_layers_dict[layer_id] = tf.range(tf_layers_dict[tf_edges[layer_id][0]][0], tf_layers_dict[tf_edges[layer_id][1]],
-                                                delta=int(tf_layers_dict[tf_edges[layer_id][2]]), dtype=dtype)
+            tf_layers_dict[layer_id] = tf.range(tf_layers_dict[tf_edges[layer_id][0]][0],
+                                                tf_layers_dict[tf_edges[layer_id][1]],
+                                                delta=int(tf_layers_dict[tf_edges[layer_id][2]]),
+                                                dtype=dtype)
 
         ### Exp
         elif layer.attrib['type'] == 'Exp':
